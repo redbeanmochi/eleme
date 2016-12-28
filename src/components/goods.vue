5f69f60be18fd67ff76b5af170b975708ff018f3
wangyouzhi <redbeanmochi@163.com>
2016-12-28 16:13:01 +0800

    -- 实现了商品列表

--- src/components/goods.vue
+++ src/components/goods.vue
@@ -2,20 +2,48 @@
   <div class="goods">
     <div class="menu-wrapper">
       <ul>
-        <li v-for="item in goods" class="menu-item">
+        <li v-for="item in goods" class="menu-item" @click="menuClick">
           <span class="text">
              <span v-show="item.type > 0" class="icon" :class="classMap[item.type]"></span>{{item.name}}
           </span>
         </li>
       </ul>
     </div>
-    <div class="food-wrapper"></div>
+    <div class="food-wrapper" id="food-wrapper">
+      <ul>
+        <li v-for="item in goods" class="food-list">
+          <h1 class="food-list-title">{{ item.name }}</h1>
+          <ul>
+            <li v-for="food in item.foods" class="food-item">
+              <div class="food-pic">
+                <img :src="food.icon" alt="">
+              </div>
+              <div class="food-content">
+                <h2 class="food-content-name">{{ food.name }}</h2>
+                <p class="food-content-description">{{ food.description }}</p>
+                <div class="extra">
+                  <span>月售{{food.sellCount}}份</span>
+                  <span>好评率{{food.rating}}%</span>
+                </div>
+                <div class="food-content-price">
+                  <span class="price">&yen;<b>{{food.price}}</b></span>
+                  <span v-show="food.oldPrice" class="old-price">&yen;{{food.oldPrice}}</span>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
   export default{
     props: ['seller'],
+    mounted: function () {
+
+    },
     created(){
       this.$http.get('/api/goods').then((res) => {
           res = res.body;
@@ -29,6 +57,18 @@
     data(){
       return {
         goods: {}
+      }
+    },
+    methods: {
+      menuClick: function (e) {
+        let text = e.currentTarget.innerText;
+        let dom_list = document.getElementsByClassName("food-list-title");
+        // 因为 safari 浏览器不支持 for of 的遍历，所以修改为 for in 遍历
+        for (let index in dom_list) {
+          if (dom_list[index].innerText == text) {
+            document.getElementById("food-wrapper").scrollTop = dom_list[index].offsetTop;
+          }
+        }
       }
     }
   };
@@ -53,8 +93,17 @@
     overflow: auto;
   }
 
+  .menu-wrapper::-webkit-scrollbar {
+    width: 0;
+  }
+
   .food-wrapper {
     flex: 1;
+    overflow: auto;
+  }
+
+  .food-wrapper::-webkit-scrollbar {
+    width: 0;
   }
 
   .menu-item {
@@ -94,5 +143,99 @@
     bottom: 0;
     border-top: 1px solid rgba(7, 17, 27, .1);
     width: 56px;
+  }
+
+  .food-list-title {
+    height: 26px;
+    line-height: 26px;
+    font-size: 12px;
+    color: rgb(147, 153, 159);
+    background: #f3f5f7;
+    border-left: 2px solid #d9dde1;
+    padding-left: 12px;
+  }
+
+  .food-item {
+    padding-bottom: 18px;
+    width: 100%;
+    display: flex;
+    position: relative;
+  }
+
+  .food-item:after {
+    content: '';
+    position: absolute;
+    left: 18px;
+    right: 18px;
+    bottom: 0;
+    border-bottom: 1px solid rgba(17, 27, 37, .1);
+  }
+
+  .food-item:last-child:after {
+    display: none;
+  }
+
+  .food-pic {
+    flex: 0 0 57px;
+    font-size: 0;
+    width: 57px;
+    height: 57px;
+    margin-top: 18px;
+    margin-left: 18px;
+  }
+
+  .food-pic img {
+    width: 57px;
+    height: 57px;
+    display: block;
+  }
+
+  .food-content {
+    flex: 1;
+    font-size: 0;
+    vertical-align: top;
+    width: 100%;
+  }
+
+  .food-content .food-content-name {
+    font-size: 14px;
+    height: 14px;
+    line-height: 14px;
+    color: rgb(7, 17, 27);
+    margin-top: 22px;
+    margin-left: 10px;
+  }
+
+  .food-content-description {
+    margin-top: 8px;
+    margin-left: 10px;
+    font-size: 10px;
+    line-height: 10px;
+    color: rgb(147, 153, 159);
+  }
+
+  .extra {
+    margin-top: 8px;
+    font-size: 10px;
+    color: rgb(147, 153, 159);
+    margin-left: 10px;
+  }
+
+  .food-content-price {
+    font-size: 10px;
+    color: #cc0000;
+    margin-left: 10px;
+    margin-right: 8px;
+    margin-top: 8px;
+  }
+
+  .food-content-price .price {
+    font-size: 14px;
+  }
+
+  .food-content-price .old-price {
+    font-size: 10px;
+    color: rgb(147, 153, 159);
+    text-decoration: line-through;
   }
 </style>
